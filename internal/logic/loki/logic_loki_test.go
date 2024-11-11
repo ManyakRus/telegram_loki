1949ae29b4cbca26d26913da3f9ebc7d51a833be
Nikitin Aleksandr <nikitinav@atomsbt.ru>
2024-11-11 11:32:57 +0300

    сделал StringDateTime

---
+++ internal/logic/loki/logic_loki_test.go
@@ -0,0 +1,21 @@
+package loki
+
+import (
+	"github.com/ManyakRus/starter/config_main"
+	"github.com/ManyakRus/telegram_loki/internal/config"
+	"github.com/ManyakRus/telegram_loki/internal/load_json"
+	"github.com/golang-module/carbon/v2"
+	"testing"
+	"time"
+)
+
+func TestStart_period(t *testing.T) {
+	config_main.LoadENV_or_SettingsTXT()
+	config.FillSettings()
+	load_json.LoadJSON_All()
+
+	Date2 := time.Now()
+	Date1 := carbon.CreateFromStdTime(Date2).AddMinutes(-200).StdTime()
+	Start_period(Date1, Date2)
+
+}
